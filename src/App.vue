2eb3aa11f9ee032ceccedb1cf3bb32f9e422e006
Tobias Norton <tnorton20@yahoo.com>
2019-01-22 09:02:31 -0500

    Initial app created, separated HTML, CSS and Vue/JS

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <Quote-Card></Quote-Card>
+  </div>
+</template>
+
+<script>
+import QuoteCard from "./components/quote-card/quote-card.component.vue";
+export default {
+  name: "app",
+  components: {
+    QuoteCard
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/quote-card/quote-card.component.vue
@@ -0,0 +1,13 @@
+<template src='./quote-card.component.html'></template>
+<style src='./quote-card.component.css'></style>
+
+<script>
+export default {
+  name: "Quote-Card",
+  data() {
+    return {
+      newQuote: "Hey guys. What's up? "
+    };
+  }
+};
+</script>
